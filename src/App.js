e351a6d99d1fbed61a7ca35b988d4fe0506b7398
JColeJ <jordzcs@gmail.com>
2023-06-22 19:36:08 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './calculator-logo.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            <b>Calculator Supreme</b>
         </p>
         <a
           className="App-link"
